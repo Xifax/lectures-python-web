212f02f456d05fe9e6ee9c3375a500bb608ab93b
Artiom Basenko <demi.log@gmail.com>
2018-07-23 17:04:32 +0300

    Update some of the sections and include additional graphics

---
+++ src/1-http-server.py
@@ -0,0 +1,16 @@
+from http.server import SimpleHTTPRequestHandler
+import socketserver
+import logging
+import os
+from pathlib import Path
+
+class Handler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.error(self.headers)
+        SimpleHTTPRequestHandler.do_GET(self)
+
+os.chdir(Path('.') / 'src')
+
+with socketserver.TCPServer(("", 8000), Handler) as httpd:
+    httpd.serve_forever()

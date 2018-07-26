6b49f0c24f9501b752e3a77cd306df18a939ca7c
Artiom Basenko <demi.log@gmail.com>
2018-07-26 15:57:11 +0300

    Modify docker example

---
+++ src/docker/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Poet(Resource):
+    def get(self):
+        return { 'poets': [
+            'John Keats',
+            'Emily Dickinson',
+            'William Wordsworth',
+            'Vladimir Nabokov',
+        ]}
+
+# Create routes
+api.add_resource(Poet, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)

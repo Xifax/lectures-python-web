652857550ab9ea52d7baaf03f22e498d148bcae7
Artiom Basenko <demi.log@gmail.com>
2018-07-30 12:06:42 +0300

    Completely rewrite advanced docker example

--- src/docker-advanced/api/app.py
+++ src/docker-advanced/api/app.py
@@ -1,20 +1,31 @@
-from flask import Flask
-from flask_restful import Resource, Api
+import datetime
+import os
+
+from flask import Flask, render_template, redirect, url_for
+# from forms import SignupForm
+#
+# from models import Signups
+# from database import db_session
 
 app = Flask(__name__)
-api = Api(app)
+app.secret_key = os.environ['APP_SECRET_KEY']
 
-class Poet(Resource):
-    def get(self):
-        return { 'poets': [
-            'John Keats',
-            'Emily Dickinson',
-            'William Wordsworth',
-            'Vladimir Nabokov',
-        ]}
+# @app.route("/", methods=('GET', 'POST'))
+# def signup():
+#     form = SignupForm()
+#     if form.validate_on_submit():
+#         signup = Signups(name=form.name.data, email=form.email.data, date_signed_up=datetime.datetime.now())
+#         db_session.add(signup)
+#         db_session.commit()
+#         return redirect(url_for('success'))
+#     return render_template('signup.html', form=form)
+@app.route('/')
+def landing():
+    return "Hello there guys!"
 
-# Create routes
-api.add_resource(Poet, '/')
+@app.route("/success")
+def success():
+    return "Thank you for signing up!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5090, debug=True)
---
+++ src/docker-advanced/client/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
+
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})

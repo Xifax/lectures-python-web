33b5a722d747fbf8d2ea624ce56170072fa89b66
Artiom Basenko <demi.log@gmail.com>
2018-09-09 19:19:09 +0300

    Update django example with models and tests

--- src/django/tasks/models.py
+++ src/django/tasks/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Common(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+class Task(Common):
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+
+    topic = models.ForeignKey('Topic',
+            on_delete=models.CASCADE,
+            related_name='tasks')
+    student = models.ForeignKey(
+            'Student',
+            on_delete=models.CASCADE,
+            related_name='tasks')
+
+    def complete(self):
+        self.done = True
+        self.save()
+        return self
+
+
+class Topic(Common):
+    pass
+
+
+class Student(Common):
+    helpers = models.ManyToManyField('self')
+
+
+class Group(Common):
+    members = models.ManyToManyField(
+        Student,
+        through='Status',
+        # order is important!
+        through_fields=('group', 'student'),
+    )
+
+
+class Status(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    leader = models.BooleanField(default=False)
